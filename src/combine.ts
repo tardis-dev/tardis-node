c38d0f0c83819c23da8724ae3c3efc678d1a43c7
Thad <thad@tardis.dev>
2019-10-11 20:56:42 +0200

    feat: working implementation of combine async iterators

--- src/combine.ts
+++ src/combine.ts
@@ -1,12 +1,15 @@
-import { Message } from './types'
-import { ONE_SEC_IN_MS, wait } from './handy'
+import { ONE_SEC_IN_MS } from './handy'
 
 type NextMessageResultWitIndex = {
   index: number
-  result: IteratorResult<Message, Message>
+  result: IteratorResult<LocallyTimestamped, LocallyTimestamped>
 }
 
-async function getNextResultWithIndex(iterator: AsyncIterableIterator<Message>, index: number): Promise<NextMessageResultWitIndex> {
+type LocallyTimestamped = { localTimestamp: Date }
+
+const DATE_MAX = new Date(8640000000000000)
+
+async function nextWithIndex(iterator: AsyncIterableIterator<LocallyTimestamped>, index: number): Promise<NextMessageResultWitIndex> {
   const result = await iterator.next()
 
   return {
@@ -30,48 +33,58 @@ function findOldestResult(oldest: NextMessageResultWitIndex, current: NextMessag
   return oldest
 }
 
-// based on https://github.com/fraxken/combine-async-iterators
 // combines multiple iterators with Messages from for example multiple exchanges
 // works both for real-time and historical data
-
-export async function* combine(...iterators: AsyncIterableIterator<Message>[]) {
-  const nextMessages = iterators.map(getNextResultWithIndex)
-  let { result } = await Promise.race(nextMessages)
+export async function* combine<T extends LocallyTimestamped>(...iterators: AsyncIterableIterator<T>[]): AsyncIterableIterator<T> {
+  if (iterators.length === 0) {
+    return
+  }
+  const nextResults = iterators.map(nextWithIndex)
+  let { result } = await Promise.race(nextResults)
   const firstReceivedMessage = result.value
   const now = new Date()
   const THREE_MINUTES_IN_MS = 3 * 60 * ONE_SEC_IN_MS
   // based on local timestamp of first message decide if iterators provide is real time data
   // if first message is less than three minutes 'old' in comparison to current time
-  // consider such iterators as real-time and return message in FIFO order - using promise.race
   const isRealTime = firstReceivedMessage.localTimestamp.valueOf() + THREE_MINUTES_IN_MS > now.valueOf()
 
   if (isRealTime) {
+    // return messages in FIFO order thanks to using Promise.race
+    // based on https://github.com/fraxken/combine-async-iterators
+
     while (true) {
       // this does not handle iterators that are finite,
       // as we'd have to handle case when result.done is set to true
       // but in practice real-time streams are infinite
-      const { index, result } = await Promise.race(nextMessages)
-      yield result.value as Message
-      nextMessages[index] = getNextResultWithIndex(iterators[index], index)
+      const { index, result } = await Promise.race(nextResults)
+      yield result.value as T
+      nextResults[index] = nextWithIndex(iterators[index], index)
     }
   } else {
-    const results = await Promise.all(nextMessages)
+    const results = await Promise.all(nextResults)
+    let aliveIteratorsCount = results.length
     do {
       // if we're deailing with historical data replay
-      // we need to return combined messages sorted by local timestamp in acending order
+      // and need to return combined messages iterable sorted by local timestamp in acending order
+
       const oldestResult = results.reduce(findOldestResult, results[0])
       const { result, index } = oldestResult
 
-      // TODO: rethink indexes!!!!
       if (result.done) {
-        // remove already done iterables from results
-        results.splice(oldestResult.index, 1)
+        aliveIteratorsCount--
+        // we don't want finished iterators to every be considered 'oldest' again
+        // hence provide them with result that has local timestamp set to DATE_MAX
+
+        results[index].result = {
+          value: {
+            localTimestamp: DATE_MAX
+          }
+        }
       } else {
         // yield oldest value and replace with next value from iterable for given index
-        yield result.value
-        const indexWithOffset = oldestResult.index - aliveIteratorsCount
-        results[indexWithOffset] = await getNextResultWithIndex(iterators[index], index)
+        yield result.value as T
+        results[index] = await nextWithIndex(iterators[index], index)
       }
-    } while (results.length > 0)
+    } while (aliveIteratorsCount > 0)
   }
 }
