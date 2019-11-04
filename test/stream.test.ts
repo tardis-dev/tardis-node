6872faed8e2c7f61a8ae75670f353a51d69fae52
Thad <thad@tardis.dev>
2019-11-04 17:27:01 +0100

    tests: update real-time data streaming tests to check for book snapshots as well

--- test/stream.test.ts
+++ test/stream.test.ts
@@ -14,42 +14,67 @@ describe('stream', () => {
   test(
     'streams normalized real-time messages for each supported exchange',
     async () => {
-      const skippedExchanges = ['bitstamp', 'binance-dex']
       for (const exchange of EXCHANGES) {
-        if (skippedExchanges.includes(exchange)) {
-          continue
+        const exchangeDetails = await getExchangeDetails(exchange)
+
+        const normalizers: any[] = [normalizeTrades]
+        // bitstamp issue under node 12 (invalid headers)
+        // let's skip book snapshots for it
+        if (exchange !== 'bitstamp') {
+          normalizers.push(normalizeBookChanges)
         }
 
-        const exchangeDetails = await getExchangeDetails(exchange)
-        const normalizers = exchangesWithDerivativeInfo.includes(exchange)
-          ? [normalizeTrades, normalizeBookChanges, normalizeDerivativeTickers]
-          : [normalizeTrades, normalizeBookChanges]
+        if (exchangesWithDerivativeInfo.includes(exchange)) {
+          normalizers.push(normalizeDerivativeTickers)
+        }
 
-        const validPrefixes = ['btc', 'xbt', 'pi_']
-        const availableSymbols = exchangeDetails.availableSymbols.filter(s =>
-          validPrefixes.some(p => s.id.toLocaleLowerCase().startsWith(p))
-        )
+        var symbols = exchangeDetails.availableSymbols
+          .filter(s => s.availableTo === undefined)
+          .slice(0, 100)
+          .map(s => s.id)
+
         const messages = streamNormalized(
           {
             exchange,
-            symbols: availableSymbols
-              .filter(s => s.availableTo === undefined)
-              .slice(0, 2)
-              .map(s => s.id)
+            symbols,
+            withDisconnectMessages: true
           },
-          ...(normalizers as any)
+          ...normalizers
         )
 
         let count = 0
-        for await (const _ of messages) {
-          if (count >= 50) {
-            break
+        let snapshots = 0
+        // check if each symbol received book snapshot
+        // and then there was at least 200 messages received after
+
+        for await (const msg of messages) {
+          // reset counters if we've received disconnect
+          if (msg.type === 'disconnect') {
+            count = 0
+            snapshots = 0
           }
-          count++
+
+          if (msg.type === 'book_change' && (msg as any).isSnapshot) {
+            snapshots++
+          }
+
+          // again bitstamp is an special on here since there's not book change messages for it
+          if (exchange === 'bitstamp') {
+            count++
+            if (count >= 5) {
+              break
+            }
+          }
+
+          if (snapshots === symbols.length) {
+            count++
+            if (count >= 200) {
+              break
+            }
+          }
         }
       }
     },
-
     1000 * 60 * 10
   )
 })
