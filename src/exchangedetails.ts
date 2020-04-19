e2e7204e39c3341dda38ccacead158c70195040d
Thaddeus <thad@tardis.dev>
2020-04-19 14:50:53 +0200

    improvement: update exchange details TS typings to include recently added fields related to datasets

--- src/exchangedetails.ts
+++ src/exchangedetails.ts
@@ -9,23 +9,55 @@ export async function getExchangeDetails<T extends Exchange>(exchange: T) {
   return exchangeDetails as ExchangeDetails<T>
 }
 
-export type ExchangeDetails<T extends Exchange> = {
+export type SymbolType = 'spot' | 'future' | 'perpetual' | 'option'
+
+export type Stats = {
+  trades: number
+  bookChanges: number
+}
+
+export type DatasetType = 'trades' | 'incremental_book_L2' | 'quotes' | 'derivative_ticker'
+
+type Datasets = {
+  dataTypes: DatasetType[]
+  formats: ['csv']
+  exportedFrom: Date
+  exportedUntil: Date
+  stats: Stats
+  symbols: {
+    id: string
+    type: SymbolType
+    availableSince: string
+    availableTo: string
+    stats: Stats
+  }[]
+}
+
+export type ExchangeDetailsBase<T extends Exchange> = {
   id: T
   name: string
-  enabled: boolean
   filterable: boolean
+  enabled: boolean
   availableSince: string
+
+  availableChannels: FilterForExchange[T]['channel'][]
+
   availableSymbols: {
     id: string
-    type: 'spot' | 'future' | 'perpetual' | 'option'
+    type: SymbolType
     availableSince: string
     availableTo?: string
+    name?: string
   }[]
-  availableChannels: FilterForExchange[T]['channel'][]
+
   incidentReports: {
     from: string
     to: string
-    status: string
+    status: 'resolved' | 'wontfix'
     details: string
   }
 }
+
+type ExchangeDetails<T extends Exchange> =
+  | (ExchangeDetailsBase<T> & { supportsDatasets: false })
+  | (ExchangeDetailsBase<T> & { supportsDatasets: true; datasets: Datasets })
