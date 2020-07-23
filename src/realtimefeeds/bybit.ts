5a3ef1738fd67d9d3510de15ea4c304650bb1e65
Thaddeus <thad@tardis.dev>
2020-07-23 15:54:31 +0200

    improvement: support bybit linear in real-time feed, closes #19

--- src/realtimefeeds/bybit.ts
+++ src/realtimefeeds/bybit.ts
@@ -1,17 +1,63 @@
 import { Filter } from '../types'
-import { RealTimeFeedBase } from './realtimefeed'
+import { RealTimeFeedBase, MultiConnectionRealTimeFeedBase } from './realtimefeed'
 
-export class BybitRealTimeDataFeed extends RealTimeFeedBase {
-  protected wssURL = 'wss://stream.bybit.com/realtime'
+export class BybitRealTimeDataFeed extends MultiConnectionRealTimeFeedBase {
+  protected *_getRealTimeFeeds(exchange: string, filters: Filter<string>[], timeoutIntervalMS?: number, onError?: (error: Error) => void) {
+    const linearContractsFilters = filters.reduce(
+      this._only((s) => s.endsWith('USDT')),
+      [] as Filter<string>[]
+    )
+
+    const inverseContractsFilters = filters.reduce(
+      this._only((s) => s.endsWith('USDT') === false),
+      [] as Filter<string>[]
+    )
+
+    if (linearContractsFilters.length > 0) {
+      yield new BybitSingleConnectionRealTimeDataFeed('realtime_public', exchange, linearContractsFilters, timeoutIntervalMS, onError)
+    }
+
+    if (inverseContractsFilters.length > 0) {
+      yield new BybitSingleConnectionRealTimeDataFeed('realtime', exchange, inverseContractsFilters, timeoutIntervalMS, onError)
+    }
+  }
+
+  private _only(filter: (symbol: string) => boolean) {
+    return (prev: Filter<string>[], current: Filter<string>) => {
+      if (!current.symbols || current.symbols.length === 0) {
+        throw new Error('BybitRealTimeDataFeed requires explicitly specified symbols when subscribing to live feed')
+      }
+
+      const symbols = current.symbols.filter(filter)
+      if (symbols.length > 0) {
+        prev.push({
+          channel: current.channel,
+          symbols
+        })
+      }
+      return prev
+    }
+  }
+}
+
+class BybitSingleConnectionRealTimeDataFeed extends RealTimeFeedBase {
+  protected readonly wssURL: string
+
+  constructor(
+    wsURLSuffix: string,
+    exchange: string,
+    filters: Filter<string>[],
+    timeoutIntervalMS?: number,
+    onError?: (error: Error) => void
+  ) {
+    super(exchange, filters, timeoutIntervalMS, onError)
+    this.wssURL = `wss://stream.bybit.com/${wsURLSuffix}`
+  }
 
   protected mapToSubscribeMessages(filters: Filter<string>[]): any[] {
     const args = filters
       .map((filter) => {
-        if (!filter.symbols || filter.symbols.length === 0) {
-          throw new Error('BybitRealTimeDataFeed requires explicitly specified symbols when subscribing to live feed')
-        }
-
-        return filter.symbols.map((symbol) => {
+        return filter.symbols!.map((symbol) => {
           const suffix = filter.channel === 'instrument_info' || filter.channel === 'orderBook_200' ? '.100ms' : ''
           return `${filter.channel}${suffix}.${symbol}`
         })
