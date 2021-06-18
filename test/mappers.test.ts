a0711029b0fdbc353df5723611a7c88c9447133f
Thaddeus <thad@tardis.dev>
2021-06-18 10:24:40 +0200

    add back missing test

--- test/mappers.test.ts
+++ test/mappers.test.ts
@@ -2862,7 +2862,8 @@ describe('mappers', () => {
           ['sell', '0.02185', '0'],
           ['sell', '0.02188', '149.57']
         ]
-      }
+      },
+      { type: 'trade', symbol: 'ETHUSD', event_id: 9608228023, timestamp: 1580919901276, price: '200.19', quantity: '100', side: 'block' }
     ]
 
     const geminiMapper = createMapper('gemini')
