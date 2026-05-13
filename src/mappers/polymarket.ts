59a40e515d23623a8752bc64c14c7a238fe126ab
marcin <marcin@tardis.dev>
2026-05-13 16:30:17 +0200

    feat(polymarket-integration): added more types

--- src/mappers/polymarket.ts
+++ src/mappers/polymarket.ts
@@ -20,12 +20,55 @@ type PolymarketMarketBookMessage = PolymarketMarketMessage<'book'> & {
   asset_id: string
   timestamp: string
   hash: string
-  bids: PolymarketLevel[]
-  asks: PolymarketLevel[]
+  bids: PolymarketMarketBookLevel[]
+  asks: PolymarketMarketBookLevel[]
   tick_size?: string
 }
 
-type PolymarketLevel = {
+type PolymarketMarketBookLevel = {
   price: string
   size: string
 }
+
+type PolymarketMarketPriceChangeMessage = PolymarketMarketMessage<'price_change'> & {
+  timestamp: string
+  price_changes: PolymarketMarketPriceChange[]
+}
+
+type PolymarketMarketPriceChange = {
+  asset_id: string
+  price: string
+  size: string
+  side: PolymarketMarketTradeSide
+  hash: string
+  best_bid: string
+  best_ask: string
+}
+
+type PolymarketMarketLastTradePriceMessage = PolymarketMarketMessage<'last_trade_price'> & {
+  asset_id: string
+  fee_rate_bps: string
+  price: string
+  side: PolymarketMarketTradeSide
+  size: string
+  timestamp: string
+  transaction_hash: string
+}
+
+type PolymarketMarketTradeSide = 'BUY' | 'SELL'
+
+type PolymarketMarketTickSizeChangeMessage = PolymarketMarketMessage<'tick_size_change'> & {
+  asset_id: string
+  old_tick_size: string
+  new_tick_size: string
+  timestamp: string
+}
+
+type PolymarketMarketBestBidAskMessage = PolymarketMarketMessage<'best_bid_ask'> & {
+  asset_id: string
+  best_bid: string
+  best_ask: string
+  spread: string
+  timestamp: string
+}
+
