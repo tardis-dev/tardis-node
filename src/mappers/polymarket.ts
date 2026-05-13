1cdbfc3eef2786e7682af72e76ecb56d911acbd3
marcin <marcin@tardis.dev>
2026-05-13 17:02:30 +0200

    feat(polymarket-integration): added more types

--- src/mappers/polymarket.ts
+++ src/mappers/polymarket.ts
@@ -23,6 +23,7 @@ type PolymarketMarketBookMessage = PolymarketMarketMessage<'book'> & {
   bids: PolymarketMarketBookLevel[]
   asks: PolymarketMarketBookLevel[]
   tick_size?: string
+  last_trade_price?: string
 }
 
 type PolymarketMarketBookLevel = {
@@ -72,3 +73,104 @@ type PolymarketMarketBestBidAskMessage = PolymarketMarketMessage<'best_bid_ask'>
   timestamp: string
 }
 
+type PolymarketMarketNewMarketMessage = PolymarketMarketMessage<'new_market'> & {
+  id: string
+  question: string
+  slug: string
+  description: string
+  assets_ids: string[]
+  outcomes: string[]
+  event_message: PolymarketEventMessage
+  timestamp: string
+  tags: string[]
+  condition_id: string
+  active: boolean
+  clob_token_ids: string[]
+  sports_market_type: string
+  line: string
+  game_start_time: string
+  order_price_min_tick_size: string
+  group_item_title: string
+  taker_base_fee?: string
+  fees_enabled?: boolean
+  fee_schedule?: PolymarketMarketFeeSchedule
+}
+
+type PolymarketEventMessage = {
+  id: string
+  ticker: string
+  slug: string
+  title: string
+  description: string
+}
+
+type PolymarketMarketFeeSchedule = {
+  exponent: string
+  rate: string
+  taker_only: boolean
+  rebate_rate: string
+}
+
+type PolymarketMarketResolvedMessage = PolymarketMarketMessage<'market_resolved'> & {
+  id: string
+  assets_ids: string[]
+  winning_asset_id: string
+  winning_outcome: string
+  timestamp: string
+  tags: string[]
+}
+
+type PolymarketSportsResultMessage = {
+  gameId?: number
+  leagueAbbreviation?: string
+  slug?: string
+  homeTeam?: string
+  awayTeam?: string
+  status?: PolymarketSportsStatus
+  eventState?: PolymarketSportsEventState
+  score: string
+  period: string
+  elapsed?: string
+  last_update?: string
+  live: boolean
+  ended: boolean
+  turn?: string
+  finished_timestamp?: string
+}
+
+type PolymarketSportsEventState = {
+  type: string
+  createdAt: string
+  updatedAt: string
+  score: string
+  period: string
+  live: boolean
+  ended: boolean
+  tournamentName?: string
+  tennisRound?: string
+}
+
+type PolymarketSportsStatus =
+  | 'Scheduled'
+  | 'InProgress'
+  | 'Final'
+  | 'F/OT'
+  | 'F/SO'
+  | 'Suspended'
+  | 'Postponed'
+  | 'Delayed'
+  | 'Canceled'
+  | 'Forfeit'
+  | 'NotNecessary'
+  | 'Break'
+  | 'PenaltyShootout'
+  | 'Awarded'
+  | 'not_started'
+  | 'running'
+  | 'finished'
+  | 'postponed'
+  | 'canceled'
+  | 'scheduled'
+  | 'inprogress'
+  | 'suspended'
+  | 'cancelled'
